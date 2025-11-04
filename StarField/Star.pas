5d3e09842e56be2a823ea81cedab8f9ac7da9d7f
milan-micic <milan2micic@yahoo.com>
2025-11-04 21:12:51 +0100

    Starfield

--- StarField/Star.pas
+++ StarField/Star.pas
@@ -2,7 +2,13 @@
 
 interface
 
-uses GraphABC;
+var speed: integer;
+
+function map(n:real; 
+             start1:real; 
+             stop1:real; 
+             start2:real; 
+             stop2:real):integer;
 
 type
   _Star = class
@@ -18,41 +24,66 @@ end;
 
 implementation
 
-function map(n:real; start1:real; stop1:real; start2:real; stop2:real):real;
+uses GraphABC;
+
+function map(n:real; 
+              start1:real; 
+              stop1:real; 
+              start2:real; 
+              stop2:real):integer;
 begin
 // ((n-start1)/(stop1-start1))*(stop2-start2)+start2  
-  map := ((n-start1)/(stop1-start1))*(stop2-start2)+start2;
+  map := trunc(((n-start1)/(stop1-start1))*(stop2-start2)+start2);
 end;
   
 procedure _Star.update();
 begin
-  
+  _z:=_z - speed;
+  if _z < 1 then begin
+    _x:=Random(-Window.Width,Window.Width);
+    _y:=Random(-Window.Height,Window.Height);
+    _z:=Window.Width;
+    _pz:=_z;
+  end;
 end;
 
 procedure _Star.show();
 var
-  sx,sy,r,px,py: real;
+  sx,sy,r,px,py: integer;
 begin
-  SetPenColor(clWhite);
+  
   SetBrushColor(clWhite);
+  SetPenWidth(0);
 
-//  sx := map(_x/_z, 0, 1, 0, Window.Width);
-//  sy := map(_y/_z, 0, 1, 0, Window.Height);
-//
-//  r := map(_z, 0, Window.Width, 16, 0);
-//  circle(Round(sx), Round(sy), Round(r));
-  Circle(_x,_y,8);
-  Writeln(_x,_y);
+  sx := map(_x/_z, 0, 1, 0, Window.Width);
+  sy := map(_y/_z, 0, 1, 0, Window.Height);
+
+  r := map(_z, 0, Window.Width, 4, 0);
+  
+//  circle(sx, sy, r);
+
+  px := map(_x/_pz, 0,1,0,Window.Width);
+  py := map(_y/_pz, 0,1,0,Window.Height);
+
+  _pz:=_z;
+
+  SetPenColor(clWhite);
+  SetPenWidth(r);
+  line(px,py,sx,sy);
 end;
 
 constructor _Star.Create();
   begin
-    _x:=Random(Window.Width);
-    _y:=Random(Window.Height);
+    
+    _x:=Random(-Window.Width,Window.Width);
+    _y:=Random(-Window.Height,Window.Height);
     _z:=Random(Window.Width);
     _pz:=_z;
+    
   end;
+  
+initialization
 
-begin
-  writeln(map(5,100,200,2,10));
+  speed:=0;
+  
 end.
--- StarField/StarField.pas
+++ StarField/StarField.pas
@@ -1,14 +1,34 @@
 ﻿uses Star,GraphABC;
 
-var zvezda: array[0..10] of _Star;
+var zvezda: array[0..2500] of _Star;
+  fEND: boolean;
+  mouseX: integer;
 
-begin
-  SetWindowSize(400, 400);
-  ClearWindow(clBlack);
-  
-  for var i:integer:=0 to 10 do begin
-  zvezda[i] := new _Star();
-  zvezda[i].show();
+procedure MouseMove(x,y,mb:integer);
+  begin
+    mouseX:= x;
   end;
   
+begin
+  
+  SetWindowSize(800, 800);
+  Coordinate.SetOrigin(Trunc(Window.Width/2), Trunc(Window.Height/2));
+  
+  for var i:integer := low(zvezda) to high(zvezda) do begin
+    zvezda[i] := new _Star();
+  end;
+  
+  repeat
+    
+    OnMouseMove:=MouseMove;
+    speed:= map(mouseX,0,Window.Width,0,50)+1;
+
+    LockDrawing;
+    ClearWindow(clBlack);
+    for var i:integer := low(zvezda) to high(zvezda) do begin
+      zvezda[i].update();
+      zvezda[i].show();
+    end;
+    Redraw;
+  until (fEND);
 end.
